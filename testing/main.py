22cb65f7105eb249937980ee5dcdc4a7cb7fa637
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-03-20 20:50:17 +0200

    binary to float working

--- testing/main.py
+++ testing/main.py
@@ -1,23 +1,51 @@
+import struct
 
 FIXED_POINT = 7
 BITS_NUM = 16
 
 
-def dec_to_binary(num):
+def change_to_signed_binary(binary):
+    if(binary[FIXED_POINT] == '.'):
+        numOfBits = BITS_NUM
+    else:
+        numOfBits = BITS_NUM+1
+    if(binary[0] == '-'):
+        binary = binary[3:]
+        return '1'+"0"*(numOfBits - len(binary)-1)+binary
+    if(binary[:2] == "0b"):
+        binary = binary.lstrip("0b")
+    return "0"*(numOfBits - len(binary))+binary
+
+
+def dec_convert(num):
     while num > 1:
         num /= 10
     return num
 
 
 def float_to_bin(number):
+    if(number == int(number)):
+        return change_to_signed_binary(format(int(number), "#0{}b".format(BITS_NUM+2)))
     num, dec = str(number).split(".")
     num = int(num)
     dec = int(dec)
-    res = format(num, "#0{}b".format(BITS_NUM+2)).lstrip("0b") + "."
+    res = bin(num).lstrip("0b") + "."
     for _ in range(FIXED_POINT):
-        num, dec = str((dec_to_binary(dec)) * 2).split(".")
+        num, dec = str((dec_convert(dec)) * 2).split(".")
         dec = int(dec)
         res += num
+    return change_to_signed_binary(res)
+
+
+def binary_to_float(binary):
+    indx = BITS_NUM-FIXED_POINT
+    temp = binary[1:indx] + binary[indx+1:]
+    res = 0
+    for i in range(BITS_NUM-1):
+        if(temp[i] == '1'):
+            res += 2**(FIXED_POINT-i)
+    if(binary[0] == '1'):
+        res *= -1
     return res
 
 
@@ -42,13 +70,14 @@ def addition(a_num, b_num):
 
 
 def main():
-    a_num = int(input("Enter 1st number (dec): "))
-    b_num = int(input("Enter 2nd number (dec): "))
-    add_result = addition(a_num, b_num)
-    mult_res = multiplication(a_num, b_num)
-    print("Dec: {} * {} = {}".format(a_num, b_num, mult_res))
-    print("Binary: {} * {} = {}".format(bin(a_num), bin(b_num), bin(mult_res)))
-    print("Hexa: {} * {} = {}".format(hex(a_num), hex(b_num), hex(mult_res)))
+    a_num = float(input("Enter 1st number (dec): "))
+    b_num = float(input("Enter 2nd number (dec): "))
+    print(binary_to_float(float_to_bin(a_num)))
+    print(binary_to_float(float_to_bin(b_num)))
+    # mult_res = multiplication(a_num, b_num)
+    # print("Dec: {} * {} = {}".format(a_num, b_num, mult_res))
+    # print("Binary: {} * {} = {}".format(bin(a_num), bin(b_num), bin(mult_res)))
+    # print("Hexa: {} * {} = {}".format(hex(a_num), hex(b_num), hex(mult_res)))
     # print("{} * {} = {}".format(hex(a_num), hex(b_num), mult_res))
 
 
