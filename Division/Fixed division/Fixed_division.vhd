505c7f6fe52decae1d981813877cdfdd03991b6b
Muhanad <muhanad.atef23@gmail.com>
2020-04-18 01:52:55 +0200

    2'S Complement Without (+)

--- Division/Fixed division/Fixed_division.vhd
+++ Division/Fixed division/Fixed_division.vhd
@@ -12,31 +12,72 @@ entity fixed_division is
 		  OverFlow : out std_logic);
 end entity;
 
+
 architecture arch of fixed_division is
 	begin
-		Quotient(N-1) <= Divisor(N-1) xor Dividend(N-1);
-		 
 		process (Divisor,Dividend)
-		variable ERR_VAR : std_logic := '0';
 		variable SHIFTING_IT : Integer := 0;
-		variable D : std_logic_vector(N-2 downto 0);
-		variable Z : std_logic_vector(N-2 downto 0);
+		variable Dividend2 : std_logic_vector(N-2 downto 0);
+		variable Divisor2 : std_logic_vector(N-2 downto 0);
+		variable FIRST_ONE : std_logic;
 		begin	
-			-- If divisor == 0 then ERROR bit = 1
-			l1 : for i in 0 to N-2 loop
-				ERR_VAR := ERR_VAR or Divisor(i);
-				if Divisor(i) = '1' then
-					SHIFTING_IT := i; -- Calculating the index of the most 1 bit
-				end if ;
-			end loop ; -- l1
-			-- Error bit
-			ERR <= not ERR_VAR;	
-			
-			D(N-2 downto N-2-SHIFTING_IT) := Divisor(N-2 downto N-2-SHIFTING_IT);
-			Z(N-2 downto N-2-SHIFTING_IT) := Dividend(SHIFTING_IT downto 0);
-			l2 : for i in 0 to 10 loop
-				
-			end loop ; -- l2
+			-- If the divisor == 1 then the OP is the same and return
+			if unsigned(Divisor(N-2 downto 0)) = 128 then
+				Quotient(N-2 downto 0) <= Dividend (N-2 downto 0);
+			-- If the divisor then ERROR bit = 1 then return
+			elsif unsigned(Divisor(N-2 downto 0)) = 0 then
+				ERR <= '1';
+			-- Normal division
+			else
+				l1 : for i in 0 to N-2 loop
+					if Divisor(i) = '1' then
+						SHIFTING_IT := i; -- Calculating the index of the most 1 bit
+					end if ;
+				end loop ; -- l1
 
+
+				-- Calculating 2's complement for Dividend if it's negative
+				FIRST_ONE := '0';
+				if Dividend(N-1) = '1' then
+					l2 : for i in 0 to N-2 loop
+						if FIRST_ONE = '1' then
+							Dividend2(i) := not Dividend(i);
+						else
+							Dividend2(i) := Dividend(i);
+						end if ;
+						if Dividend2(i) = '1' then
+							FIRST_ONE := '1';
+						end if ;
+					end loop ; -- l2
+				else
+					Dividend2 := Dividend(N-2 downto 0);
+				end if ;
+				-- Calculating 2's complement for Divisor if it's negative
+				FIRST_ONE := '0';
+				if Divisor(N-1) = '1' then
+					l3 : for i in 0 to N-2 loop
+						if FIRST_ONE = '1' then
+							Divisor2(i) := not Divisor(i);
+						else
+							Divisor2(i) := Divisor(i);
+						end if ;
+						if Divisor(i) = '1' then
+							FIRST_ONE := '1';
+						end if ;
+					end loop ; -- l3
+				else
+					Divisor2 := Divisor(N-2 downto 0);
+				end if ;
+				
+				
+				-- Divide with long division
+				
+				
+
+			end if ; 
 		end process;
+
+		-- Sign of the Qoutient
+		Quotient(N-1) <= Divisor(N-1) xor Dividend(N-1);
+
 end architecture;
