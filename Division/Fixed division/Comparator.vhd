bb8626ae1d1ada137950923430fbf99a7a406f30
Muhanad <muhanad.atef23@gmail.com>
2020-04-17 17:46:42 +0200

    Minor Changes In Division Comparator

--- Division/Fixed division/Comparator.vhd
+++ Division/Fixed division/Comparator.vhd
@@ -2,20 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 
+-- Returns "1" if A >= B , "0" if A < B
 entity Comparator is
 generic(N : integer := 16);
 port(
     A: in std_logic_vector(N - 1 downto 0);
     B: in std_logic_vector(N - 1 downto 0);
-    OP: out std_logic_vector(1 downto 0)
+    OP: out std_logic
 );
 end entity;
 
 architecture DataFlow of Comparator is
 begin
 
-    OP <= "00" when A = B else
-            "11" when A > B else
-            "10" when A < B;
+    OP <= '1' when A >= B else
+            '0';
 
 end DataFlow ; -- DataFlow
