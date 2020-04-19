7940079d3765eb74950391cf4f2e348ada8129df
HossamAh <hossamahmed201515@gmail.com>
2020-04-20 01:43:04 +0200

    interpolation1

--- Interpolation/interPolation_Devision.vhd
+++ Interpolation/interPolation_Devision.vhd
@@ -15,14 +15,16 @@ end Interpolation_Devision;
 
 architecture Interpolation_DevisionArch of Interpolation_Devision is
     signal temp,TkComp,Tk_Tn,Tz_Tn,Division_output :std_logic_vector(width-1 downto 0):=(others=>'0');
-    signal Div_EN,ERROR,Div_OVF:std_logic; 
-
+    signal ERROR,Div_OVF:std_logic; 
+    signal Div_EN,DoneSignal :std_logic;
     begin
-       D1:entity work.fixed_division port map(Dividend=>Tk_Tn,Divisor=>Tz_Tn,Enable=>Div_EN,CLK=>CLK,Quotient=>Division_output,ERR=>ERROR,Done=>Done,OverFlow=>Div_OVF);  
-       DIV_EN <= '0' when Tk_Tn = (std_logic_vector(to_unsigned(0,width))) and Tz_Tn = (std_logic_vector(to_unsigned(0,width)))
-        else '1' ;
+       D1:entity work.fixed_division port map(Dividend=>Tk_Tn,Divisor=>Tz_Tn,Enable=>Div_EN,CLK=>CLK,Quotient=>Division_output,ERR=>ERROR,Done=>DoneSignal,OverFlow=>Div_OVF);  
+        DIV_EN <= '1' when (Tk_Tn /= (std_logic_vector(to_unsigned(0,width))) and Tz_Tn /= (std_logic_vector(to_unsigned(0,width)))) and falling_edge(CLK) else
+        '0' when (rising_edge(CLK));
         temp <= not Tn;
         TkComp  <= std_logic_vector(unsigned(temp + 1));
         sub1:entity work.Carry_Look_Ahead(Behavioral) port map(A=>Tk,B=>TkComp,Cin=>'0',S=>Tk_Tn);
         sub2:entity work.Carry_Look_Ahead(Behavioral) port map(A=>Tz,B=>TkComp,Cin=>'0',S=>Tz_Tn);
+        Done<= DoneSignal;
+        DivOut<=(others=>'Z') when (DoneSignal = '0'and EN ='0')else Division_output; 
         end architecture;
