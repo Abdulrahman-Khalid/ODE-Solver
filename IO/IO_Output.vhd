25a0593d1716b1bd688233401380846d79c05b46
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-04-22 17:13:10 +0200

    ready to test io

--- IO/IO_Output.vhd
+++ IO/IO_Output.vhd
@@ -13,54 +13,59 @@ entity IO_Output is
     memory_data : in std_logic_vector(bus_bits-1 downto 0);
     CPU_Bus : inout std_logic_vector(N-1 downto 0);
     memory_address : out integer;
-    done_processing : out std_logic);
+    done_output_data : out std_logic);
 end entity;
 
 architecture arch of IO_Output is
     begin
     process (CLK) is 
     variable start_Output_address : integer; -- Ram 1
-    variable Starting_T : integer; -- Ram 1
-    variable Is_Completed_64_bit : std_logic;
-    variable flip_to_output_address : std_logic; -- For getting the packet bit value 
+    variable start_T_address : integer; -- Ram 1
+    variable state : std_logic(1 downto 0);
     begin
         if RST = '1' then
             Starting_T := 5003;
             start_Output_address := 2503;
             flip_to_output_address := '0';
-            Is_Completed_64_bit := '0';
-            Done_Processing <= '0';
-        end if ;
-        if Enable_Output_IO = '1' and RST = '0' then
-            if (rising_edge(clk)) then
-                if(Starting_T <= end_T_address+1 and Is_Completed_64_bit = '0') then
-                    -- Read from mem
+            state := "00";
+            Done_output_data <= '0';
+        elsif (Enable_Output_IO = '1') then
+            if (rising_edge(clk) and state = "00") then
+                if(Starting_T <= end_T_address) then
                     memory_address <= Starting_T;
                     Starting_T := Starting_T + 1;
-                else
-                    -- Read from mem
-                    flip_to_output_address := '1';
-                end if;
-                if(start_Output_address /= end_Output_address and flip_to_output_address = '1') then
+                    Done_output_data <= '0';
+                elsif(start_Output_address <= end_Output_address) then
                     memory_address <= start_Output_address;
-                    -- Read from mem
                     start_Output_address := start_Output_address + 1;
+                    Done_output_data <= '0';
                 else
+                    state := "10";
                     memory_address <= start_Output_address;
-                    Done_Processing <= '1';
+                    Done_output_data <= '0';
                 end if;
-                if Is_Completed_64_bit = '0' then
+            elsif(falling_edge(clk)) then
+                if state = "00" then
                     CPU_Bus <= memory_data(bus_bits-1 downto bus_bits/2);
-                    Is_Completed_64_bit := '1';
+                    state := "01";
+                    Done_output_data <= '0';
+                elsif(state = "01") then
+                    CPU_Bus <= memory_data((bus_bits/2)-1 downto 0);
+                    state := "00";
+                    Done_output_data <= '0';
+                elsif(state = "10") then -- "10"
+                    CPU_Bus <= memory_data((bus_bits/2)-1 downto 0);
+                    state := "11";
+                    Done_output_data <= '0';
                 else
-                    CPU_Bus <= memory_data(bus_bits/2-1 downto 0);
-                    Is_Completed_64_bit := '0';
+                    Done_output_data <='1';
                 end if ;
             end if;
         else
             Starting_T := 5003;
             start_Output_address := 2503;
-            flip_to_output_address := '0';
+            state := "00";
+            Done_output_data <= '0';
         end if ;
     end process;
 
