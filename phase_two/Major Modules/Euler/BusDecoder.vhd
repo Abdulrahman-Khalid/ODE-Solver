1c3aeb962ec56479069acff190f1fcf4639d984f
MhmdAbdlh <47702978+MhmdAbdlh@users.noreply.github.com>
2020-04-24 22:01:11 +0200

    Euler edits, Phase2 Design reporrts Folder

--- phase_two/Major Modules/Euler/BusDecoder.vhd
+++ phase_two/Major Modules/Euler/BusDecoder.vhd
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+library ieee;
+use ieee.std_logic_1164.all;
+
+Entity Decoder is
+	port(s : IN std_logic_vector (2 downto 0);
+	     firstRaw : IN std_logic;
+	     d : out std_logic_vector (1 to 10));
+end Decoder;
+
+architecture dec of Decoder is
+begin
+	process(s)
+	begin
+		if(s="000") then
+ 		       d <= "0000000000";
+		elsif (s="001") then
+		       d <= "1000000010";
+
+		elsif (s="010") then
+		       d<="0110000000";
+		   
+		elsif (s="011") then
+		       d<="0000010000";
+		       
+		elsif (s="100") then
+		       if firstRaw = '1' then d<= "0001000100"; else d <= "0001101000"; end if;
+ 
+		elsif (s="101") then
+		       d<="0000100000";
+		       
+		elsif (s="110") then
+		       d<="0000000010";
+		elsif (s="111") then
+		       d<="0000000011";
+		end if;
+	end process;
+end dec;
+=======
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -33,3 +72,4 @@ begin
 		end if;
 	end process;
 end dec;
+>>>>>>> 48c080a5d275053c5171d803ee54a2e6341d4d50
