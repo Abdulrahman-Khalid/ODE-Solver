993159b2c8bbfa185f4483b92500b1f0aa6dc706
HossamAh <hossamahmed201515@gmail.com>
2020-04-26 03:33:59 +0200

    interpolation in code

--- phase_two/Code/Interpolation/Interpolation.vhd
+++ phase_two/Code/Interpolation/Interpolation.vhd
@@ -18,8 +18,8 @@ begin
     DivDoneFlag <= DivisonDoneSignal;
     Dk:entity work.Interpolation_Devision port map (Tk=>Tk,Tz=>Tz,Tn=>Tn_Reg,clk=>clk,rst=>rst,EN=>DivFlag,Done=>DivisonDoneSignal,DivOut=>Div_Output);
     L1:entity work.Interpolation_Logic port map (Un=>Un,Uz=>Uz,DivResult=>Div_Output_Reg,En=>DivisonDoneSignal,Uk=>Uk);
-    Reg_Tz:entity work.reg generic map(N=>width) port map(D=>Tz,Load=>'1',CLK=>CLK,Q=>Tz_RegTemp,rst=>reset);
-    Reg_Tn:entity work.reg generic map(N=>width) port map(D=>Tz_Reg,Load=>shiftFlag,CLK=>clk,Q=>Tn_RegTemp,rst=>reset);
+    Reg_Tz:entity work.reg generic map(N=>width) port map(D=>Tz,Load=>'1',CLK=>CLK,Q=>Tz_RegTemp,rst=>rst);
+    Reg_Tn:entity work.reg generic map(N=>width) port map(D=>Tz_Reg,Load=>shiftFlag,CLK=>clk,Q=>Tn_RegTemp,rst=>rst);
     DivOut_Reg :entity work.reg generic map(N=>width) port map(D=>Div_Output,Load=>DivisonDoneSignal,CLK=>clk,Q=>Div_Output_Reg,rst=>rst);
     Tn_reg <= Tn_RegTemp when shiftFlag ='1' else (others=>'0') when rst ='1';
     Tz_reg <= Tz_RegTemp ;
